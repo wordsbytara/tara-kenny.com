7d9f64b0b9d0fa564f32ad9d8dbbc4e84499d9a7
Sam Hains <sdh@eml.cc>
2018-01-08 19:36:23 -0500

    add javascript

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,40 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+let  toggleElement = (id) => {
+    var element = document.getElementById(id);
+  if (element.classList) {
+      element.classList.toggle("hide");
+  } else {
+      // For IE9
+      var classes = element.className.split(" ");
+      var i = classes.indexOf("hide");
+
+      if (i >= 0)
+          classes.splice(i, 1);
+      else
+          classes.push("hide");
+          element.className = classes.join(" ");
+  }
+}
+
+window.toggleAcademic = function() {
+  toggleElement("academic-list")
+}
+
+window.toggleEssays = function() {
+  toggleElement("essays-list")
+}
+
+window.toggleFiction = function() {
+  toggleElement("fiction-list")
+}
+
+window.toggleOther = function() {
+  toggleElement("other-list")
+}
+
+window.toggleInterviews = function() {
+  toggleElement("interviews-list")
+}
